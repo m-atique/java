955c02e8bc2d4e742fb9190ba3f04b3a10f32eb1
m-atique <atiquenwl@gmail.com>
2020-06-13 16:46:08 +0500

    04 chapters

--- app.js
+++ app.js
@@ -1,2 +1,137 @@
-alert("Error! Please enter a valid passward")
-alert("Welcome to the JS Code" + "br" + "Happy Coding")
+
+
+
+// //============================================ 
+// //            Chapter # 1
+// //============================================
+
+// //___________ Task # 1________________
+
+// alert("Error! Please enter a valid passward");
+
+
+// //___________ Task # 2________________
+
+// alert("Welcome to the JS land" +"\n"+
+//  " Happy Coding!");
+
+// //___________ Task # 3________________
+
+// alert("Welcome to the JS land...");
+
+// //___________ Task # 4________________
+
+// alert("Happy Codding" + "\n"+ "Prevent this page creating additional dialog");
+
+// //___________ Task # 5________________
+
+// console.log("Hello, I can run JS through my web browser's console")
+
+// ===================================================================================
+
+
+//============================================ 
+//            Chapter # 2
+//============================================
+
+// //___________ Task # 1________________
+
+// var username;
+
+// //___________ Task # 2________________
+
+
+// var myName;
+
+// //___________ Task # 3________________
+// var messsage;
+// messsage = "Hello World";
+// alert(messsage);
+
+
+// //___________ Task # 4________________
+// var student_name;
+// var student_age;
+// var student_course;
+// student_name= "Muhammad Atique";
+// student_age = "30 years Old";
+// student_course = "Certified Mobile Application development";
+// alert(student_name);
+// alert(student_age);
+// alert(student_course);
+
+// //___________ Task # 5________________
+// var triangle;
+// triangle ="Pizza" + "\n" + "Pizz"+ "\n" + "Piz"+ "\n" + "Pi"+ "\n" + "P";
+// alert(triangle);
+
+
+// //___________ Task # 6________________
+// var email;
+// email = "doctor@live .com"
+// alert("My email Address is "+ email)
+
+// //___________ Task # 7________________
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the Book" + book );
+
+// //___________ Task # 8________________
+// document.write("YAh! I can write HTML content through JavaScript");
+
+ 
+
+// //___________ Task # 9________________
+// var decoration = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+//  alert(decoration)
+
+//  =================================================================================
+                                    // CHAPTER 03
+// ===================================================================================
+
+// //___________ Task # 1________________
+// var age = 30;
+// alert("I am "+ age  +" " +"year old");
+
+// //___________ Task # 2________________
+// var site_visit = 30;
+// alert("You have visited his" +" " + site_visit + " "+ "times");
+
+// //___________ Task # 3________________
+// var birthYear = 1987;
+// document.write("My birth year is " + birthYear + "<br>"+ "Data Type of my declared veriable is numer");
+// //___________ Task # 4________________
+// var site = "XYZ Clothing Store "
+// var Visitor_name =" Jhon Doe";
+// var Product = "T-shirt";
+// var Quantity = 5;
+// document.write(Visitor_name + "orders" + Quantity + " "+"(s) on" +" " + site)
+// // =====================================================================================
+
+//  =================================================================================
+                                    // CHAPTER 04
+// ===================================================================================
+//___________ Task # 1________________
+var name;
+var age;
+var course;
+//___________ Task # 2________________
+
+// -------- 05 illegle veriables-------------
+// var 1st_name;
+// var father's name;
+// var section-Headers;
+// var alret 02;
+// var peach@TreeWalker;
+// -------- 05 legle veriables-------------
+var first_name;
+var father_name;
+var section_Headers;
+var alret_02;
+var peach_at_TreeWalker;
+
+//___________ Task # 3________________
+document.write("<h2>" + "Rules for naming JS variables" + "</h2>" + "<br><br><br>")
+
+document.write("<p>" + "Variable names can only contain $,numbers,and _.For example $my_1stVariable" + "<br>"
++  "Variables must begin with a letter,$ or _.For example $name, _name or name" + "<br>"
++ "Variable names are case sensitive"+ "<br>" + "Variable names should not be JS keyword" + "<p>") 
