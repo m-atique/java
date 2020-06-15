39b46151eaf04845cba238912422a029b43f61f1
m-atique <atiquenwl@gmail.com>
2020-06-15 12:30:41 +0500

    11 chapters

--- app.js
+++ app.js
@@ -111,27 +111,245 @@
                                     // CHAPTER 04
 // ===================================================================================
 //___________ Task # 1________________
-var name;
-var age;
-var course;
-//___________ Task # 2________________
+// var name;
+// var age;
+// var course;
+// //___________ Task # 2________________
 
-// -------- 05 illegle veriables-------------
-// var 1st_name;
-// var father's name;
-// var section-Headers;
-// var alret 02;
-// var peach@TreeWalker;
-// -------- 05 legle veriables-------------
-var first_name;
-var father_name;
-var section_Headers;
-var alret_02;
-var peach_at_TreeWalker;
+// // -------- 05 illegle veriables-------------
+// // var 1st_name;
+// // var father's name;
+// // var section-Headers;
+// // var alret 02;
+// // var peach@TreeWalker;
+// // -------- 05 legle veriables-------------
+// var first_name;
+// var father_name;
+// var section_Headers;
+// var alret_02;
+// var peach_at_TreeWalker;
 
-//___________ Task # 3________________
-document.write("<h2>" + "Rules for naming JS variables" + "</h2>" + "<br><br><br>")
+// //___________ Task # 3________________
+// document.write("<h2>" + "Rules for naming JS variables" + "</h2>" + "<br><br><br>")
 
-document.write("<p>" + "Variable names can only contain $,numbers,and _.For example $my_1stVariable" + "<br>"
-+  "Variables must begin with a letter,$ or _.For example $name, _name or name" + "<br>"
-+ "Variable names are case sensitive"+ "<br>" + "Variable names should not be JS keyword" + "<p>") 
+// document.write("<p>" + "Variable names can only contain $,numbers,and _.For example $my_1stVariable" + "<br>"
+// +  "Variables must begin with a letter,$ or _.For example $name, _name or name" + "<br>"
+// + "Variable names are case sensitive"+ "<br>" + "Variable names should not be JS keyword" + "<p>")
+
+//  =================================================================================
+                                    // CHAPTER 05
+// ===================================================================================
+//___________ Task # 1________________
+// var x = 3;
+// var y = 5;
+// var z = x + y;
+// document.write("Sum of"+" "+ x  + " "+ "and"+" "+y + " " +"is equal to"+z + "<br>")
+
+// //___________ Task # 2________________
+// var a = y-x;
+// document.write("Subtraction of"+" "+ y  + " "+ "and"+" "+x + " " +"is equal to"+a  + "<br>");
+// var b = x*y;
+// document.write("Multiplication of"+" "+ x  + " "+ "and"+" "+y + " " +"is equal to"+b  + "<br>");
+// var c = x/y;
+// document.write("Division of"+" "+ x  + " "+ "and"+" "+y + " " +"is equal to"+c  + "<br>");
+// var d = y%x;
+// document.write("Reminder of"+" "+ y  + " "+ "modulus"+" "+x + " " +"is equal to"+d  + "<br>");
+
+// //___________ Task # 3________________
+// var num;
+// document.write("Value after visiablae declaration is:"+"  "+ num + "<br>");
+// num = 5;
+// document.write("Value after intial declaration is:"+"  "+ num + "<br>");
+// num = ++num;
+// document.write("Value after icreament is:"+"  "+ num + "<br>");
+// num += 7;
+// document.write("Value after addition is:"+"  "+ num + "<br>");
+// num = --num;
+// document.write("Value after decrement is"+"  "+ num + "<br>");
+// var output = num%3;
+// document.write("Output:"+"  "+ output);
+
+// //___________ Task # 4________________
+// var movie_ticket = 600;
+// Total_price = 600*5;
+
+// document.write("Total cast of buying 05 tickets for movies  is:"+"  "+ Total_price + "PKR"+"<br>");
+
+// //___________ Task # 5________________
+// var table_num = 4;
+// document.write("<h3>"+"Table of 04" + "</h3>")
+// document.write( table_num + " "+"x"+ " "+ "1"  +" "+"="+ " "+ 4*1+ "<br>")
+// document.write( table_num + " "+"x"+ " "+ "2"  +" "+"="+ " "+ 4*2+ "<br>")
+// document.write( table_num + " "+"x"+ " "+ "3"  +" "+"="+ " "+ 4*3+ "<br>")
+// document.write( table_num + " "+"x"+ " "+ "4"  +" "+"="+ " "+ 4*4+ "<br>")
+// document.write( table_num + " "+"x"+ " "+ "5"  +" "+"="+ " "+ 4*5+ "<br>")
+// document.write( table_num + " "+"x"+ " "+ "6"  +" "+"="+ " "+ 4*6+ "<br>")
+// document.write( table_num + " "+"x"+ " "+ "7"  +" "+"="+ " "+ 4*7+ "<br>")
+// document.write( table_num + " "+"x"+ " "+ "8"  +" "+"="+ " "+ 4*8+ "<br>")
+// document.write( table_num + " "+"x"+ " "+ "9"  +" "+"="+ " "+ 4*9+ "<br>")
+// document.write( table_num + " "+"x"+ " "+ "10"  +" "+"="+ " "+ 4*10+ "<br>")
+
+// //___________ Task # 6________________
+// document.write("<h2>"+ "Temprature Unit Convertion"+"</h2>")
+// var calcius = 37;
+// var degree_to_forn = (calcius *9/5)+32;
+// document.write(calcius+ "<sup>" + "o"+"</sup>" + "C" + " "+"is equal to " +" "+ degree_to_forn + "<sup>" + "o"+"</sup>" + "F" +"<br>" );
+// var forn = 77;
+// forn_to_degree =(forn-32)*5/9;
+// document.write(forn + "<sup>" + "o"+"</sup>" + "F" +" "+"is equal to " +" "+ forn_to_degree + "<sup>" + "o"+"</sup>" + "C"+"<br>" )
+// //___________ Task # 7________________
+// var Price_of_item_1 = 100;
+// var Price_of_item_2 = 200;
+// var Ordered_quantity_of_item_1 = 5;
+// var Ordered_quantity_of_item_2 =10;
+// var Shipping_charges = 2000;
+// document.write("price of item# 01 is:"+" " + Price_of_item_1 + "<br>");
+// document.write("Ordered quantity of item# 01 is:"+" " + Ordered_quantity_of_item_1 + "<br>");
+// document.write("price of item# 02 is:"+" " + Price_of_item_2 + "<br>");
+// document.write("Ordered quantity of item# 02 is:"+" " + Ordered_quantity_of_item_2 + "<br>");
+// document.write("Shippinng Charges:"+" " + Shipping_charges + "<br>");
+// var Total = (Price_of_item_1*Ordered_quantity_of_item_1) +(Price_of_item_2*Ordered_quantity_of_item_2)+Shipping_charges;
+
+// //_________________Task # 8____________________
+// document.write("<h2>"+ "Marks Sheet"+"</h2>");
+// var obtained_marks = 681;
+// var total_marks = 850;
+// var percentage = obtained_marks*100/total_marks;
+// document.write("Obtained marks by student:"+ " "+ obtained_marks +"<br>")
+// document.write("Total marks :"+ " "+ total_marks + "<br>")
+// document.write("Percentage:"+ " "+ percentage + "%"+"<br>")
+
+// //_________________Task # 09____________________
+// document.write("<h2>" + "Currency to PKR"+ "</h2>")
+// var suadi_riyal =25;
+// var us_doller = 10;
+// var suadi_riyal_rate = 28;
+// var us_doller_rate = 104.80;
+// var total =  (suadi_riyal*suadi_riyal_rate)+(us_doller*us_doller_rate);
+// document.write("Total currency in PKR:" + " "+ total);
+
+//_________________Task # 09____________________
+// var num_basic =10;
+// var result = ((num_basic+5)*10)/2
+// document.write("<h2>" + "Calculations"+ "</h2>")
+// document.write("Result :"+ result)
+
+// //_________________Task # 10____________________
+// var birth_year =1987;
+// var current_year = 2020;
+// var your_age = current_year-birth_year;
+// document.write("<h2>" + "Age Calculator"+ "</h2>")
+// document.write("Birth year :"+ birth_year  +"<br>")
+// document.write("Current year :"+ current_year +"<br>")
+// document.write("Your Age is :"+ your_age +"<br>")
+
+// //_________________Task # 10____________________
+// var radius =20;
+// var pai = 3.142;
+// var circumference = 2*pai*radius;
+// var Area_circle = pai*radius**2;
+// document.write("<h2>" + "The Geometrizer"+ "</h2>");
+// document.write("Radius :"+ radius +"<br>")
+// document.write("Circumference of  Circle :"+ circumference  +"<br>")
+// document.write("Area of Circle  :"+ Area_circle +"<br>")
+
+// //_________________Task # 10____________________
+// var snack = "choclate chip";
+// var max_age =63;
+// var current_age = 33;
+// var remaining_age = max_age-current_age;
+// var req_snaks_per_day = 3;
+// var total_snaks = req_snaks_per_day*remaining_age;
+// document.write("<h2>" + "The The Lifetime Supply Calculator:"+ "</h2>");
+// document.write("Favorite Snack :"+ snack +"<br>")
+// document.write("Estimated age :"+ remaining_age  +"<br>")
+// document.write("Estimated amount per day :"+ req_snaks_per_day +"<br>")
+// document.write("You will need" +" "+ req_snaks_per_day*remaining_age +" "+ "to last you until the ripe old age of "+" "+ max_age);
+//  =================================================================================
+                                    // CHAPTER 06-09
+// ===================================================================================
+// //___________ Task # 1________________
+// var a = 10;
+// document.write("The value of a is:"+ " "+ a  +"<br>" +"<br>")
+
+// var plus_a = ++a;
+// document.write("The value of ++a is:"+ " "+ plus_a +"<br>")
+// document.write("The value of a is:"+ " "+ a +"<br>" +"<br>");
+
+// var a_plus = a++
+// document.write("The value of a++ is:"+ " "+ a_plus +"<br>")
+// document.write("The value of a is:"+ " "+ a +"<br>" +"<br>");
+
+// var minus_a =--a;
+// document.write("The value of --a is:"+ " "+ minus_a +"<br>")
+// document.write("The value of a is:"+ " "+ a +"<br>" +"<br>");
+
+// var a_minus = a--;
+// document.write("The value of a-- is:"+ " "+ a_minus +"<br>")
+// document.write("The value of a is:"+ " "+ a +"<br>" +"<br>");
+
+
+// //___________ Task # 2________________
+// var a=2, b=1;
+// document.write("The value of a:"+ " " + a +"<br>");
+// document.write("The value of b:"+ " " + b +"<br>");
+
+// var step_01 = --a;
+// document.write("value of --a:"+" "+ step_01 +"<br>");
+
+// var step_02 = step_01 - --b;
+// document.write("value of --a - --b:"+" "+ step_02  +"<br>");
+
+// var step_03 = step_02 + ++b ;
+// document.write("value of --a - --b + ++b:"+" "+ step_03  +"<br>");
+
+// var step_04 = step_03 + b--;
+// document.write("value of --a - --b + ++b + b--:"+" "+step_04 +"<br>");
+
+// document.write("The result is:"+""+step_04 + "<br>")
+
+// //___________ Task # 3________________
+//  user_name =prompt("Please Enter your Name")
+//  document.write("Welcome"+" " + user_name+ " "+ "to the new era of technology")
+
+// //___________ Task # 04-05________________
+//  table_num2 =prompt("Please Enter your Nember",5)
+//  document.write("<h3>"+"Table of"+ " "+table_num2 + "</h3>")
+// document.write( table_num2 + " "+"x"+ " "+ "1"  +" "+"="+ " "+ table_num2*1+ "<br>")
+// document.write( table_num2 + " "+"x"+ " "+ "2"  +" "+"="+ " "+ table_num2*2+ "<br>")
+// document.write( table_num2 + " "+"x"+ " "+ "3"  +" "+"="+ " "+ table_num2*3+ "<br>")
+// document.write( table_num2 + " "+"x"+ " "+ "4"  +" "+"="+ " "+ table_num2*4+ "<br>")
+// document.write( table_num2 + " "+"x"+ " "+ "5"  +" "+"="+ " "+ table_num2*5+ "<br>")
+// document.write( table_num2 + " "+"x"+ " "+ "6"  +" "+"="+ " "+ table_num2*6+ "<br>")
+// document.write( table_num2 + " "+"x"+ " "+ "7"  +" "+"="+ " "+ table_num2*7+ "<br>")
+// document.write( table_num2 + " "+"x"+ " "+ "8"  +" "+"="+ " "+ table_num2*8+ "<br>")
+// document.write( table_num2 + " "+"x"+ " "+ "9"  +" "+"="+ " "+ table_num2*9+ "<br>")
+// document.write( table_num2 + " "+"x"+ " "+ "10"  +" "+"="+ " "+ table_num2*10+ "<br>")
+
+//___________ Task # 04-05________________
+var subject1 =prompt("Enter your 1st subject");
+var subject2 =prompt("Enter your 2nd subject");
+var subject3 =prompt("Enter your 3rd subject");
+
+var total_marks =100;
+
+var subject1_marks =prompt("Enter your obtained marks in"+ " "+" "+ subject1);
+var subject2_marks =prompt("Enter your obtained marks in"+" " +" "+ subject2);
+var subject3_marks =prompt("Enter your obtained marks in"+ " " + subject3);
+
+var headings = ("<b>" + "Subjects" + "<b>" +"   "+ "Total marks" + "  "+ "Obtained marks"+"    "+ "Percentage" +   "%" + "<br>")
+var result_subject1 = ("<b>" + subject2 + "<b>" +"------   "+ total_marks + " ------ "+ subject2_marks+" ------   "+ subject2_marks*100/total_marks +   "%" + "<br>")
+var result_subject2 = ("<b>" + subject2 + "<b>" +"------   "+ total_marks + " ------ "+ subject2_marks+"  ------  "+ subject2_marks*100/total_marks +   "%" + "<br>")
+var result_subject3 = ("<b>" + subject3 + "<b>" +"------   "+ total_marks + " ------ "+ subject3_marks+"  ------  "+ subject3_marks*100/total_marks +   "%" + "<br>")
+var Total_result = ("<b>" + "Total" + "<b>" +" ------  "+ total_marks*3 + "------  "+ subject3_marks+subject2_marks+subject1_marks +"  ------  "+ subject3_marks+subject2_marks+subject1_marks*100/total_marks*3 +   "%" + "<br>")
+
+document.write(headings)
+document.write(result_subject1)
+document.write(result_subject1)
+document.write(result_subject3)
+document.write(Total_result)
+
+//  =================================================================================
+                                    // CHAPTER 09-11
+// ===================================================================================
+// //___________ Task # 1________________
