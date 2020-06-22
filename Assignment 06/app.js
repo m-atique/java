5d917d96a793ebaa520aa8b820f9c0a072c43943
m-atique <atiquenwl@gmail.com>
2020-06-22 18:18:44 +0500

    date methods up to 34 chpter

--- Assignment 06/app.js
+++ Assignment 06/app.js
@@ -120,7 +120,7 @@
 // document.write("number of occurrences of word the is"+" "+ number +"<br>")
 
 //  =================================================================================
-                                    // CHAPTER 26-30        (rounding number and control string)
+                                    // CHAPTER 26-30        (MATH METHODS)
 // ===================================================================================
 //  //___________ Task # 1________________
 // var number = 3.45214
@@ -187,24 +187,174 @@
 // document.write("Your weight is:"+" "+ weight_int);
 // }
 
+// //___________ Task # 8________________
+// var my_num = Math.random()*10
+// my_num= Math.floor(my_num)
+
+// var User_num = prompt("Enter your  number")
+
+// if(User_num === my_num){
+//     document.write( "Congragulation win the Game")
+// }else
+// document.write("OOOOPS You Loss the Game")
+
+//  =================================================================================
+                                    // CHAPTER 31-35        (DATE METHODA)
+// ===================================================================================
+var mS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec']
+//  //___________ Task # 1________________
+// document.write(Date())
+
+// //___________ Task # 2________________
+//  var month_name = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+//  var d =  new Date();
+//  var  month = d.getMonth();
+//  document.write("The current month is  " + month_name[month])
+
+// //___________ Task # 3________________
+// var d_large =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// var d_small =["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var d = new Date();
+// var day = d.getDay()
+// document.write("The current Day is  "+ d_small[day])
+//___________ Task # 4________________
+// var d_large =["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// var d_small =["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+// var d = new Date();
+// var day = d.getDay()
+// if(day == 0|| day == 6){
+//     document.write("It is Fun Day")
+// }else
+// document.write("The current Day is  "+ d_small[day])
+
+// //___________ Task # 5________________
+//  var d = new Date()
+//  var  day_no = d.getDate();
+//  if(day_no <= 15){
+//  document.write("First fifteen days of the month")
+//  }else
+//  document.write("Last days of the Month")
+
+// //___________ Task # 6________________
+// var d = new Date();
+// var milli_sec =d.getTime();
+// document.write("The elapsed milli seconds since 01 jan 1970 =  "+milli_sec)
+
+// //___________ Task # 7________________
+// var d = new Date();
+// var hours = d.getHours()
+// if(hours <= 12){
+//     document.write("<h2>"+"It is AM"+"</h2>")
+// }else
+// document.write("<h2>"+"It is PM"+"</h2>")
+
+// //___________ Task # 8________________
+// var laterdate = new Date("2020, Dec, 31")
+// document.write("<h2>"+"Later Date: "+"</h2>"+laterdate);
+
+// //___________ Task # 9________________
+// var upcoming_ramzan = new Date("2021, Apr, 15");
+// var up_sec = upcoming_ramzan.getTime()
+// var last_ramzam = new Date ("2020, Apr, 24");
+// var last_sec = last_ramzam.getTime();
+// var difference = up_sec -last_sec
+// var rem_days = difference/(1000*60*60*24)
+// document.write("Days remainin in upcomimg ramzan = " +rem_days);
+
+// //___________ Task # 10________________
+// var ref_date = new Date("2020, Apr, 15");
+// var ref_sec = ref_date.getTime()
+// var ref2_date = new Date ("2020, jan, 01");
+// var ref2_sec = ref2_date.getTime();
+// var difference = ref_sec - ref2_sec
+// var rem_days = difference/(1000*60)
+// document.write("On reference date "+ ref_date +" " +rem_days +" has been passed away since 2015");
+
+// //___________ Task # 11________________
+
+// var d = new Date();
+// document.write("Current Date: " + d +"<br>")
+// var d_milli = d.getTime();
+// var d_milli2 = d_milli - 3600000;
+// var d2 = new Date(d_milli2)
+// document.write(" and  01 hour ago It is "+ d2 )
+
+// //___________ Task # 12________________
+// var d = new Date();
+// document.write("Current Date: " + d +"<br>")
+// var d_milli = d.getTime();
+// var d_milli2 = ((d_milli/(1000*60*60*24*365))-100)
+// var d_milli3 = d_milli2*1000*60*60*24*365
+// var d2 = new Date(d_milli3)
+// document.write(" and  01 hour ago It is "+ d2 )
+
+// //___________ Task # 14________________
+//  var current_date = new Date();
+//  var dob = new Date(prompt("Enter your date of birth","YYYY, MM, DD"))
+//  var birth_year = dob.getFullYear()
+//  dob = dob.getTime()
+//  current_date2 =current_date.getTime();
+//  var difference =current_date - dob
+
+//  var age = difference/(1000*60*60*24*365)
+//  age = Math.floor(age)
+//  document .write("Your Age is: "+ age +"<br>");
+//  document.write("You birth year is: "+ birth_year)
+ 
+//___________ Task # 15________________
+
+var Customer_Name = "Waqar Ahmad"
+var Current_Month =  "June"
+var Number_of_units = 480
+var Charges_per_unit = 15
+var Net_Amount_Payable_indate = (Number_of_units*Charges_per_unit)
+Net_Amount_Payable_indate = Number(Net_Amount_Payable_indate.toFixed(2))
+var Late_Payment_Surcharge = 350
+var Gross_Amount_Payable_after =  (Net_Amount_Payable_indate + Late_Payment_Surcharge)
+Gross_Amount_Payable_after = Number(Gross_Amount_Payable_after.toFixed(2))
+document.write("<h2>"+ "K-ELECTIC BILL"+"</h2>")
+document.write("Customer Name: " + Customer_Name +"<br>")
+document.write("Billing Month: " +Current_Month +"<br>")
+document.write("Number of units: " + Number_of_units +"<br>")
+document.write("Charges per Unit: " + Charges_per_unit +"<br>"+"<br>")
+document.write("Net Amount Payable (within Due Date) : " + Net_Amount_Payable_indate +"<br>")
+document.write("Charges per unit: " + Late_Payment_Surcharge +"<br>")
+document.write("Gross Amount Payable (after Due Date): " + Gross_Amount_Payable_after +"<br>")
+
+//  =================================================================================
+                                    // CHAPTER 36-38        (DATE METHODA)
+// ===================================================================================
+//  //___________ Task # 1________________
+
+
+//___________ Task # 2________________
+
+
+
+//___________ Task # 3________________
+
+//___________ Task # 4________________
+
+
+
+//___________ Task # 5________________
+
+//___________ Task # 6________________
+
+
+
+//___________ Task # 7________________
+
+
 //___________ Task # 8________________
-var my_num = Math.random()*10
-my_num= Math.floor(my_num)
-
-var User_num = prompt("Enter your  number")
-
-if(User_num === my_num){
-    document.write( "Congragulation win the Game")
-}else
-document.write("OOOOPS You Loss the Game")
-
-
 
 
 
 //___________ Task # 9________________
 
 
-
 //___________ Task # 10________________
+
+
+
 
